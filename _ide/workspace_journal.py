cf639c3edf26db1804ba8514c6da3d4ff56c1018
vikira <seowon2191@gmail.com>
2026-09-09 19:58:33 +0900

    archive.zip add

--- _ide/workspace_journal.py
+++ _ide/workspace_journal.py
@@ -7,3 +7,5 @@ client.set_workspace(path="DPU_Project")
 comp = client.get_component(name="DPU_HLS")
 comp.run(operation="SYNTHESIS")
 
+status = client.export_projects(components = ["DPU_HLS"], system_projects = [], include_build_dir = False, dest = "D:\project\DPU_Project\archive.zip")
+
